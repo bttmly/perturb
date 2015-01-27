3d5138756b509abc8ac2752fd6ada4c899b60904
Nick Bottomley <nhbottomley@gmail.com>
2015-01-27 09:48:07 -0800

    trying to sanely catch module compilation errors when req'd by mocha

--- example/test/models/vehicle-test.js
+++ example/test/models/vehicle-test.js
@@ -1,24 +1,38 @@
+"use strict";
+
 var assert = require("assert");
 
 var Vehicle = require("../../lib/models/vehicle");
 
 describe("Vehicle", function () {
+  
+  var v;
+
+  beforeEach(function () {
+    v = new Vehicle();
+  });
+
   it("is a function", function () {
     assert(typeof Vehicle === "function");
   });
 
   it("new vehicles have position of `0`", function () {
-    var v = new Vehicle();
     assert(v.position === 0);
   });
 
   it("vehicles have speed of `5` (in prototype)", function () {
-    var v = new Vehicle();
     assert(v.speed === 5);
   });
 
   it("vehicles have maxPassengers of `2` (in prototype)", function () {
-    
+    assert.equal(v.maxOccupants, 2);
+    assert.equal(v.hasOwnProperty("maxOccupants"), false);
+  });
+
+  it("vehicles can be boarded by occupants", function () {
+    v.board("abc");
+    assert.equal(v.occupants.length, 1);
+    assert.equal(v.occupants[0], "abc");
   });
 
 });
--- test/mutators-test.js
+++ test/mutators-test.js
@@ -1,2 +1,10 @@
+var expect = require("chai").expect;
+
 var getMutatorForNode = require("../lib/mutators");
 
+describe("exported function", function () {
+  it("exports a function", function () {
+    // console.log(Object.keys(getMutatorForNode));
+    expect(getMutatorForNode).to.be.a("function");
+  });
+});
