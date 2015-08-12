01b379bb5981a8e8cc7f549ec01c8f25073a5110
Nick Bottomley <nhbottomley@gmail.com>
2015-08-12 09:40:38 -0700

    cleaving apart more utils

--- test/util/map-mirror.js
+++ test/util/map-mirror.js
@@ -2,20 +2,19 @@
 
 var expect = require("chai").expect;
 
-var util = require("../lib/pkg-util");
-
+var mapMirror = require("../../lib/util/map-mirror")
 
 describe("#mapMirror", function () {
 
   it("the result is frozen", function () {
-    var result = util.mapMirror({});
+    var result = mapMirror({});
     expect(Object.isFrozen(result)).to.equal(true);
   });
 
   describe("array input", function () {
     it("returns an object with each array member mirrored as key/value", function () {
       var arr = ["one", "two", "three"];
-      var result = util.mapMirror(arr);
+      var result = mapMirror(arr);
       expect(result).to.deep.equal({
         one: "one",
         two: "two",
@@ -26,8 +25,8 @@ describe("#mapMirror", function () {
     it("throws an exception if a member isn't a string", function () {
       var arr = ["one", "two", 3];
       expect(function () {
-        util.mapMirror(arr);
-      }).to.throw("Needs a string");
+        mapMirror(arr);
+      }).to.throw("Accepts only strings");
     });
 
   });
@@ -35,7 +34,7 @@ describe("#mapMirror", function () {
   describe("object input", function () {
     it("returns an object with each object key mirrored as key/value", function () {
       var obj = {one: 1, two: 2, three: 3};
-      var result = util.mapMirror(obj);
+      var result = mapMirror(obj);
       expect(result).to.deep.equal({
         one: "one",
         two: "two",
@@ -44,20 +43,4 @@ describe("#mapMirror", function () {
     });
   });
 
-});
-
-describe("#mutantIsDead", function () {
-  it("just returns the boolean cast of the input's `failed` property", function () {
-    expect(util.mutantIsDead({failed: 0})).to.equal(false);
-    expect(util.mutantIsDead({failed: 1})).to.equal(true);
-  });
-});
-
-describe("#mutantIsAlive", function () {
-  it("passedCount is truthy and not failed", function () {
-    expect(util.mutantIsAlive({passedCount: 0, failed: true})).to.equal(false);
-    expect(util.mutantIsAlive({passedCount: 0, failed: false})).to.equal(false);
-    expect(util.mutantIsAlive({passedCount: 1, failed: true})).to.equal(false);
-    expect(util.mutantIsAlive({passedCount: 1, failed: false})).to.equal(true);
-  });
 });
