b07f104c824c97c96b130ffb62dda5a0f9cf1beb
Nick Bottomley <nhbottomley@gmail.com>
2018-10-25 00:52:12 +0000

    move a bunch of crap around;

--- src/mutators/reverse-function-parameters.ts
+++ src/mutators/reverse-function-parameters.ts
@@ -2,18 +2,12 @@ import S from "./_syntax";
 import * as util from "./util";
 import { MutatorPlugin } from "../types";
 
-const FUNC_NODES = [
-  S.FunctionDeclaration,
-  S.FunctionExpression,
-  S.ArrowFunctionExpression,
-];
-
 // reverse the perameter order for a function expression or declaration
 // `function fn (a, b) {}` => `function fn (b, a) {}`
 const plugin: MutatorPlugin = {
   type: "mutator",
   name: "reverse-function-parameters",
-  nodeTypes: FUNC_NODES,
+  nodeTypes: S.FUNC_NODES,
   filter: util.lengthAtPropGreaterThan("params", 1),
   mutator: util.update("params", (ps: any[]) => ps.slice().reverse()),
 };
